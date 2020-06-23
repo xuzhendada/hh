3a60d497920634387567e68ec4963ab2680588ff
wbxuzhen <wbxuzhen@centaline.com.cn>
2020-06-23 16:42:33 +0800

    创建组件

---
+++ my/src/main/java/com/xz/my/My.kt
@@ -0,0 +1,12 @@
+package com.xz.my
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+}
