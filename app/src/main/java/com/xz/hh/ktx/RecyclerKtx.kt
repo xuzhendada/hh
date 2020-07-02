5b946857ed2445643df92c3b58bf95031b7b103b
wbxuzhen <wbxuzhen@centaline.com.cn>
2020-07-02 10:42:34 +0800

    pageAdapter

--- app/src/main/java/com/xz/hh/ktx/RecyclerKtx.kt
+++ app/src/main/java/com/xz/hh/ktx/RecyclerKtx.kt
@@ -1,8 +1,14 @@
 package com.xz.hh.ktx
 
+import android.content.Context
 import com.xz.hh.adapter.RecyclerSupport
 import com.xz.hh.adapter.StableAdapter
+import com.xz.hh.paging.PageAdapter
 
 fun createStableAdapter(support: RecyclerSupport.() -> Unit): StableAdapter {
     return StableAdapter(RecyclerSupport().apply(support))
+}
+
+fun createPageAdapter(support: RecyclerSupport.() -> Unit, context: Context): PageAdapter {
+    return PageAdapter(RecyclerSupport().apply(support), context)
 }
--- app/src/main/java/com/xz/hh/paging/PageAdapter.kt
+++ app/src/main/java/com/xz/hh/paging/PageAdapter.kt
@@ -1,28 +1,57 @@
 package com.xz.hh.paging
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagedListAdapter
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.AdapterListUpdateCallback
-import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.DiffUtil
 import com.xz.hh.adapter.ItemCell
 import com.xz.hh.adapter.RecyclerSupport
 import com.xz.hh.adapter.RecyclerVH
 
-class PageAdapter(private val support: RecyclerSupport) : PagingDataAdapter<ItemCell, RecyclerVH>(
-    object : DiffUtil.ItemCallback<ItemCell>() {
-        override fun areItemsTheSame(oldItem: ItemCell, newItem: ItemCell) =
-            oldItem.itemId() == newItem.itemId() && oldItem.itemId() == newItem.itemId()
+class PageAdapter(private val support: RecyclerSupport, context: Context) :
+    PagingDataAdapter<ItemCell, RecyclerVH>(DIFF) {
 
-        override fun areContentsTheSame(oldItem: ItemCell, newItem: ItemCell) =
-            oldItem.itemContent() == newItem.itemContent()
+    private val layoutInflater by lazy {
+        LayoutInflater.from(context)
     }
-) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerVH {
-        TODO("Not yet implemented")
+        return RecyclerVH(
+            itemView = layoutInflater.inflate(viewType, parent, false),
+            support = support
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerVH, position: Int) {
+        //empty
+    }
+
+    override fun getItemViewType(position: Int) = getItem(position)!!.layoutId()
+
+    override fun onBindViewHolder(holder: RecyclerVH, position: Int, payloads: MutableList<Any>) {
+        holder.bind(getItem(position)!!, payloads)
+    }
+
+    companion object {
+        private val config: AsyncDifferConfig<ItemCell> = AsyncDifferConfig.Builder(
+            object : DiffUtil.ItemCallback<ItemCell>() {
+                override fun areItemsTheSame(oldItem: ItemCell, newItem: ItemCell): Boolean {
+                    return oldItem.itemId() == newItem.itemId() && oldItem.itemId() == newItem.itemId()
+                }
+
+                override fun areContentsTheSame(oldItem: ItemCell, newItem: ItemCell): Boolean {
+                    return oldItem.itemContent() == newItem.itemContent()
+                }
+            }
+        ).build()
+        private val DIFF = object : DiffUtil.ItemCallback<ItemCell>() {
+            override fun areItemsTheSame(oldItem: ItemCell, newItem: ItemCell) =
+                oldItem.itemId() == newItem.itemId() && oldItem.itemId() == newItem.itemId()
+
+            override fun areContentsTheSame(oldItem: ItemCell, newItem: ItemCell) =
+                oldItem.itemContent() == newItem.itemContent()
+        }
     }
 }
