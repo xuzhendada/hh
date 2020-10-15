89c28794bfcb51c84c5f68e916eb70ee4f1a53cf
wbxuzhen <wbxuzhen@centaline.com.cn>
2020-10-15 18:02:08 +0800

    加个Log

--- main/src/main/java/com/hi/main/page/PageSource.kt
+++ main/src/main/java/com/hi/main/page/PageSource.kt
@@ -1,16 +1,18 @@
 package com.hi.main.page
 
 
+import android.util.Log
 import androidx.paging.PagingSource
 import com.hi.common.api.ApiService
 import com.hi.common.data.response.Article
 import java.lang.Exception
 
-class  PageSource : PagingSource<Int, Article>() {
+class PageSource : PagingSource<Int, Article>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
         return try {
             val page = params.key ?: 0
             val result = ApiService.mApi.getHomeArticle(page)
+            Log.d("TAG", "当前页$page")
             LoadResult.Page(
                 data = result.data.datas,
                 prevKey = null,
