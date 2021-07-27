a1f75c7def4c289649ea52b187387df0fa98b68e
wbxuzhen <wbxuzhen@centaline.com.cn>
2021-07-27 17:31:30 +0800

    撸了个线程池

--- main/src/main/java/com/hi/main/page/ArticleRepository.kt
+++ main/src/main/java/com/hi/main/page/ArticleRepository.kt
@@ -4,7 +4,5 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 
 class ArticleRepository {
-    fun getArticle() = Pager(PagingConfig(20)) {
-        PageSource()
-    }.flow
+    fun getArticle() = Pager(PagingConfig(20)) { PageSource() }.flow
 }
